9887731e047507e130d6334d56782f5c31bc38a2
Jorge <jorgedch@gmail.com>
2019-12-09 21:06:38 +0100

    Simplify constants

--- logos_recognition/constants.py
+++ logos_recognition/constants.py
@@ -11,47 +11,44 @@ import torch
 # Global #
 ##########
 
-PATH = os.path.join(os.environ['HKT'], 'Logos-Recognition')
+PATH_GLOB = os.path.join(os.environ['HKT'], 'Logos-Recognition')
 PATH_DATA = os.path.join(os.environ['DATASETS'], 'logos')
+VIDEO_FILENAME = os.path.join(PATH_GLOB, 'data', 'test_video_small.mp4')
+name, extension = os.path.splitext(VIDEO_FILENAME)  # extension includes the '.'
+OUTPUT_NAME = VIDEO_FILENAME.replace(extension, '_output.mp4')
 PATH_EXEMPLARS = os.path.join(PATH_DATA, 'exemplars')
-LOAD_NAME = os.path.join(PATH, 'data', 'football_redbull_small.mp4')
-name, extension = os.path.splitext(LOAD_NAME)  # extension includes the '.'
-OUTPUT_NAME = LOAD_NAME.replace(extension, '_output.mp4')
+# BRAND_LOGOS = ['pepsi', 'redbull', 'heineken', 'stellaartois']
+BRAND_LOGOS = ['redbull']
+EXEMPLARS_PATHS = [os.path.join(PATH_EXEMPLARS, logo + '.jpg')
+               for logo in BRAND_LOGOS]
+IMAGE_RESIZE = (100, 100)
+USE_CLASSIFIER = True
 
 ############
 # Detector #
 ############
 
-DETECTOR_WEIGHTS = os.path.join(PATH, 'models', 'detector.pth')
-DETECTOR_DEVICE = 'cuda:2'
+DETECTOR = 'detectors.faster_rcnn'
+DETECTOR_ALG = 'binary_fasterrcnn_resnet50'
+DETECTOR_WEIGHTS = os.path.join(PATH_GLOB, 'models', 'detector.pth')
+DETECTOR_DEVICE = 'cuda:1'
 MIN_CONFIDENCE = 0.9
-IMG_SIZE = 100
 
 ##############
 # Classifier #
 ##############
 
-CLASSIFIER_WEIGHTS = os.path.join(PATH, 'models', 'classifier.pth')
-EXEMPLARS_PATH = os.path.join(PATH_DATA, 'exemplars_siamese.zip')
-REPRESENTER_WEIGHTS = os.path.join(PATH, 'models', 'siamese_embedding.pth')
-REPRESENTER_DEVICE = 'cuda:3'
+REPRESENTER_ALG = 'siamese_resnet18'
+REPRESENTER_WEIGHTS = os.path.join(PATH_GLOB, 'models', 'siamese_embedding.pth')
+REPRESENTER_DEVICE = 'cuda:2'
+REPRESENTER_IMG_SIZE = 100
+
+CLASSIFIER = 'classifiers.knn'
+CLASSIFIER_ALG = 'KNeighborsClassifier'
+CLASSIFIER_WEIGHTS = os.path.join(PATH_GLOB, 'models', 'classifier.pth')
+CLASSIFIER_DEVICE = 'cuda:3'
+
+PATH_EXEMPLARS_EMBEDDINGS = os.path.join(PATH_DATA, 'exemplars_siamese.zip')
 EMBEDDING_SIZE = 345
 DISTANCE = 'cosine'  # {cosine, minkowski_1, minkowski_2}
 MAX_DISTANCE = 0.015  # {siamese-cosine: 0.013}
-
-###########
-# PyTorch #
-###########
-
-DEVICE = torch.device('cuda:1') if os.uname()[1] == 'Shannon' \
-    else torch.device('cuda:0')
-
-#########
-# Logos #
-#########
-
-# BRAND_LOGOS = ['pepsi', 'redbull', 'heineken', 'stellaartois']
-BRAND_LOGOS = ['redbull']
-QUERY_LOGOS = [os.path.join(PATH_EXEMPLARS, logo + '.jpg')
-               for logo in BRAND_LOGOS]
-IMAGE_RESIZE = (100, 100)
