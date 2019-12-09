ebf2893ce39e8d890aa56bb101524ec36acd4a6f
Jorge <jorgedch@gmail.com>
2019-12-09 21:05:39 +0100

    Detector agent

---
+++ logos_recognition/detectors/faster_rcnn.py
@@ -0,0 +1,55 @@
+"Detector module."
+
+# Standard library:
+import os
+
+# Pip packages:
+import numpy as np
+import torch
+
+# Current library:
+from logos_recognition import detectors
+from logos_recognition.utils import image_to_gpu_tensor
+from logos_recognition.constants import (DETECTOR_DEVICE, DETECTOR_ALG,
+                                         DETECTOR_WEIGHTS, MIN_CONFIDENCE)
+
+
+
+class Detector():
+    "Add documentation."
+
+    def __init__(self):
+        "Add documentation."
+        self.model = detectors.__dict__[
+            DETECTOR_ALG](DETECTOR_DEVICE, DETECTOR_WEIGHTS)
+
+    @torch.no_grad()
+    def predict(self, image):
+        "Add documentation."
+        image = image_to_gpu_tensor(image, DETECTOR_DEVICE)
+        # Always returns list with one element:
+        detections = self.model(image)[0]
+        return self._process_detections(detections)
+
+    def _process_detections(self, detections):
+        "Add documentation."
+        # Move to cpu:
+        detections = self._detections_to_cpu(detections)
+        # keep the ones above some (potential) threshold
+        selections = np.array(detections['scores'] > MIN_CONFIDENCE)
+        return self._select_detections(detections, selections)
+
+    def _detections_to_cpu(self, detections):
+        "Moves all the fields of 'detections' to the CPU."
+        detections['boxes'] = detections['boxes'].cpu().numpy()
+        detections['labels'] = detections['labels'].cpu().numpy()
+        detections['scores'] = detections['scores'].cpu().numpy()
+        return detections
+
+    def _select_detections(self, detections, selections):
+        "Add documentation."
+        detections['boxes'] = detections['boxes'][selections]
+        detections['labels'] = detections['labels'][selections]
+        detections['scores'] = detections['scores'][selections]
+        detections['brands'] =[]
+        return detections
