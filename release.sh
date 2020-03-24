72e50664f0138e27d5224599894bc0672117c1c0
Max Pumperla <max.pumperla@googlemail.com>
2020-03-24 14:04:03 +0100

    release script

---
+++ release.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# remove old wheels
+sudo rm -rf dist/*
+
+# Build Python 2 & 3 wheels for current version
+sudo python2 setup.py sdist bdist_wheel
+sudo python3 setup.py sdist bdist_wheel
+
+twine upload dist/*
