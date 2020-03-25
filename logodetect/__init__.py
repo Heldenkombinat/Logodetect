b0081b0bef438cc448c00f3a6a47b49c49bf9369
Max Pumperla <max.pumperla@googlemail.com>
2020-03-25 16:49:22 +0100

    init external config

--- logodetect/__init__.py
+++ logodetect/__init__.py
@@ -1,2 +1,11 @@
-from .recognizer import Recognizer
-from .constants import *
+import os
+import sys
+
+if "LOGOS_RECOGNITION" in os.environ:
+    PATH_GLOB = os.environ["LOGOS_RECOGNITION"]
+else:
+    PATH_GLOB = os.path.expanduser("~/.hkt/logodetect")
+
+# We append the base path for constants.py to the Python path dynamically,
+# so we can import it project-wide.
+sys.path.append(os.path.abspath(PATH_GLOB))
