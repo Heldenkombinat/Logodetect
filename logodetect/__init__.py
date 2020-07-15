d99332541ac830a91953a0dad6b789a4c1b4be7a
Jorge <jorgedch@gmail.com>
2020-07-15 20:51:26 +0200

    Use os.path.join

--- logodetect/__init__.py
+++ logodetect/__init__.py
@@ -4,7 +4,8 @@ import sys
 if "LOGOS_RECOGNITION" in os.environ:
     PATH_GLOB = os.environ["LOGOS_RECOGNITION"]
 else:
-    PATH_GLOB = os.path.expanduser("~/.hkt/logodetect")
+    PATH_GLOB = os.path.expanduser(os.path.join(
+        os.environ["HOME"], ".hkt", "logodetect"))
 
 # We append the base path for constants.py to the Python path dynamically,
 # so we can import it project-wide.
