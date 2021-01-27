3063a41dcde02f47cf402dcbda92dd8beb57992c
Max Pumperla <max.pumperla@googlemail.com>
2021-01-27 13:19:06 +0100

    more testing

--- tests/test_recognizer.py
+++ tests/test_recognizer.py
@@ -2,7 +2,36 @@ import pytest
 import os
 
 from constants import PATH_EXEMPLARS, IMAGE_FILENAME, VIDEO_FILENAME
-from logodetect.recognizer import Recognizer
+from logodetect.recognizer import Recognizer, append_to_file_name
+
+import cv2
+
+
+@pytest.mark.unit
+def test_file_appender():
+    output_appendix = "_foo"
+    output_filename = append_to_file_name(VIDEO_FILENAME, output_appendix)
+    assert "test_video_small_50ms_foo.mp4" in output_filename
+
+
+@pytest.mark.unit
+def test_recognizer_basics():
+    reco = Recognizer(PATH_EXEMPLARS)
+
+    reco.set_exemplars(PATH_EXEMPLARS)
+    assert reco.exemplars_path == PATH_EXEMPLARS
+
+    reco.set_video_source(VIDEO_FILENAME)
+
+    output_appendix = "_foo"
+    image = cv2.imread(IMAGE_FILENAME)
+    reco.save_image(
+        image=image, image_filename=IMAGE_FILENAME, output_appendix=output_appendix
+    )
+
+    output_filename = append_to_file_name(IMAGE_FILENAME, output_appendix)
+    assert os.path.exists(output_filename)
+    os.remove(output_filename)
 
 
 @pytest.mark.unit
@@ -12,6 +41,7 @@ def test_recognizer_image():
     app.predict_image(IMAGE_FILENAME, output_appendix=output_appendix)
     name, extension = os.path.splitext(IMAGE_FILENAME)
     output_filename = IMAGE_FILENAME.replace(extension, f"{output_appendix}{extension}")
+
     assert os.path.exists(output_filename)
     os.remove(output_filename)
 
@@ -23,5 +53,6 @@ def test_recognizer_video():
     app.predict(VIDEO_FILENAME, output_appendix=output_appendix)
     name, extension = os.path.splitext(VIDEO_FILENAME)
     output_filename = VIDEO_FILENAME.replace(extension, f"{output_appendix}{extension}")
+
     assert os.path.exists(output_filename)
     os.remove(output_filename)
---
+++ tests/test_utils.py
@@ -0,0 +1,2 @@
+def test_utils():
+    pass
