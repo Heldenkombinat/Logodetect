78f1d64f1c457b5eeb0e416596961a95bc726701
Max Pumperla <max.pumperla@googlemail.com>
2021-01-27 10:42:39 +0100

    training constants

---
+++ training/loreta/constants.py
@@ -0,0 +1,11 @@
+"""Kept only for future reference, soon to be removed"""
+
+import os
+
+
+##########
+# Global #
+##########
+
+PATH = os.path.join(os.environ["HKT"], "loreta")
+CONFIG_FILE = os.path.join(PATH, "configs", "detector.json")
